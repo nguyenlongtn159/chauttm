dfc11456c76298be1b780a70010080221c692dc8
chauttm <chauttm.vnu@gmail.com>
2015-01-27 20:24:28 +0700

    done mapping Warehouse-Address

---
+++ app/models/Address.java
@@ -0,0 +1,25 @@
+package models;
+
+import play.db.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+/**
+ * Created by dse on 1/27/15.
+ */
+@Entity
+public class Address extends Model {
+    @Id
+    public Long id;
+
+    @OneToOne(mappedBy = "address")
+    public Warehouse warehouse;
+
+    public String street;
+    public String number;
+    public String postalCode;
+    public String city;
+    public String country;
+}
--- app/models/Warehouse.java
+++ app/models/Warehouse.java
@@ -5,6 +5,7 @@ import play.db.ebean.Model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +18,9 @@ public class Warehouse extends Model {
     public Long id;
 
     public String name;
+
+    @OneToOne
+    public Address address;
 
     @OneToMany(mappedBy = "warehouse")
     public List<StockItem> stock = new ArrayList();
